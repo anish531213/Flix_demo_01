93a0651163ac55dd8a55823d24c6dcf0964422bc
Anish Adhikari <anish531213@gmail.com>
2018-01-30 23:04:16 -0500

    Added collection view

---
+++ Flix_demo_01/ViewControllers/SuperheroViewController.swift
@@ -0,0 +1,94 @@
+//
+//  SuperheroViewController.swift
+//  Flix_demo_01
+//
+//  Created by Anish Adhikari on 1/29/18.
+//  Copyright © 2018 Anish Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class SuperheroViewController: UIViewController, UICollectionViewDataSource {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var movies: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.dataSource = self
+        fetchMovies()
+        // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
+        let movie = movies[indexPath.item]
+        
+        if let posterPathString = movie["poster_path"] as? String {
+            let baseUrlString = "https://image.tmdb.org/t/p/w500"
+            let posterUrl = URL(string: baseUrlString+posterPathString)!
+            cell.posterImageView.af_setImage(withURL: posterUrl)
+        }
+        return cell
+        
+    }
+    
+    func fetchMovies() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=946e1a7e73e67b8395a09bcc57800281")!
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 10)
+        
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request) { (data, response, error) in
+            // This will run when network request returns
+            if let error = error {
+                
+                let alert = UIAlertController(title: "Cannot Get Movies", message: "The internet connection appears to be offline", preferredStyle: .alert)
+                
+                let cancelAction = UIAlertAction(title: "Try Again", style: .cancel) { (action) in
+                    
+                    self.fetchMovies()
+                    // handle cancel response here. Doing nothing will dismiss the view.
+                }
+                alert.addAction(cancelAction)
+                self.present(alert, animated: true, completion: nil)
+                
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                self.movies = movies
+                self.collectionView.reloadData()
+//                self.refreshControl.endRefreshing()
+            }
+        }
+        
+        task.resume()
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Flix_demo_01/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_demo_01
+//
+//  Created by Anish Adhikari on 1/30/18.
+//  Copyright © 2018 Anish Adhikari. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
